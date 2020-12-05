b169979e3688cb032c345b822730f3ae123ed1ba
Dinh-Hoang Nam <dinhhoangnam162793@gmail.com>
2020-12-05 19:59:50 +0700

    :pencil2: Verify: working on UI

--- src/views/Verify/index.js
+++ src/views/Verify/index.js
@@ -1,4 +1,8 @@
-import { Box, Button, makeStyles, TextField } from "@material-ui/core";
+import { makeStyles, TextField } from "@material-ui/core";
+import { useSnackbar } from "notistack";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router";
+import { setDecodedData } from "../redux";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -7,7 +11,7 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    backgroundImage: "linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url('/static/images/background/1.jpg')",
+    // backgroundImage: "linear-gradient( rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4) ), url('/static/images/background/1.jpg')",
     backgroundSize: "cover",
     backgroundPosition: "center top",
   },
@@ -15,40 +19,46 @@ const useStyles = makeStyles((theme) => ({
     flexBasis: "70%",
     display: "flex",
   },
-  textField: {
-    flexGrow: 1,
-  },
-  btnContainer: {
-    flexShrink: 0,
-    padding: theme.spacing(0, 2),
-    display: "flex",
-    alignItems: "flex-end",
-  },
-  button: {},
 }));
+
 export default function Verify(props) {
   const cls = useStyles();
+  const dp = useDispatch();
+  const navigate = useNavigate();
+  const { enqueueSnackbar } = useSnackbar();
+
+  // const [tokenValue, setTokenValue] = useState("");
+
+  // function hdChange(e) {
+  //   setTokenValue(e.target.value);
+  // }
+
+  // async function hdKeyPress(e) {
+  //   if (e.key === "Enter") {
+  //   }
+  // }
+
+  async function hdPasteToken(e) {
+    const token = e.clipboardData.getData("Text");
+    const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/decode-token`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ token }),
+    });
+    const result = await response.json();
+    if (!response.ok) {
+      enqueueSnackbar("Token không hợp lệ: " + JSON.stringify(result), { variant: "error", anchorOrigin: { vertical: "top", horizontal: "center" } });
+    } else {
+      dp(setDecodedData(result));
+      navigate("/ket-qua");
+    }
+  }
+
   return (
     <div className={cls.root}>
       <div className={cls.box}>
-        <TextField
-          variant="outlined"
-          label="Token"
-          fullWidth
-          autoFocus
-          className={cls.textField}
-          onKeyPress={(e) => {
-            if (e.key === "Enter") {
-              console.log("value", e.target.value);
-              alert("enter");
-            }
-          }}
-        ></TextField>
-        <div className={cls.btnContainer}>
-          <Button variant="contained" color="primary">
-            Xác thực
-          </Button>
-        </div>
+        {/* <TextField label="Token" fullWidth autoFocus onPaste={hdPasteToken} onChange={hdChange} onKeyPress={hdKeyPress}></TextField> */}
+        <TextField label="Token" fullWidth autoFocus onPaste={hdPasteToken}></TextField>
       </div>
     </div>
   );
