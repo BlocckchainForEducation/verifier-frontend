8c877cf851053586a07fec6deae2ca73abe14abc
Dinh-Hoang Nam <dinhhoangnam162793@gmail.com>
2020-12-06 08:43:22 +0700

    :sparkles: add offchain validate logic

--- src/views/Result/index.js
+++ src/views/Result/index.js
@@ -1,5 +1,8 @@
 import { Container, makeStyles } from "@material-ui/core";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import View from "../../shared/View";
+import { setOffChainCheckResult } from "../redux";
 import DecryptedCertInfo from "./DecryptedCertInfo";
 import DecryptedSubjectTable from "./DecryptedSubjectTable";
 
@@ -13,6 +16,27 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Result(props) {
   const cls = useStyles();
+  const subjects = useSelector((state) => state.appSlice.decodedData.subjects);
+  const dp = useDispatch();
+
+  useEffect(() => {
+    runOffChainCheck();
+  }, []);
+
+  function runOffChainCheck() {
+    let sumCredit = 0;
+    let sumPoint = 0;
+    subjects.map((subject, index) => {
+      const copyVersions = [...subject.versions];
+      copyVersions.sort((a, b) => b.timestamp - a.timestamp);
+      const newestVersion = copyVersions[0];
+      sumCredit += newestVersion.credit;
+      sumPoint += newestVersion.credit * newestVersion.pointBase4;
+    });
+    let CPA = sumPoint / sumCredit;
+    dp(setOffChainCheckResult({ sumCredit, CPA }));
+  }
+
   return (
     <View title="Kết quả xác thực">
       <div className={cls.root}>
--- src/views/redux.js
+++ src/views/redux.js
@@ -8,6 +8,10 @@ const appSlice = createSlice({
       certificate: { address: "", versions: [{ txid: "", timestamp: 1234, active: "", plain: "" }] },
       subjects: [{ address: "", versions: [{ txid: "", timestamp: 1234, active: "", plain: "" }] }, {}],
     },
+    offChainCheckResult: {
+      sumCredit: 0,
+      CPA: 0,
+    },
   },
   reducers: {
     setDecodedData: (state, action) => {
@@ -27,8 +31,12 @@ const appSlice = createSlice({
         version.timestamp = action.payload.timestamp;
       }
     },
+    setOffChainCheckResult: (state, action) => {
+      state.offChainCheckResult.sumCredit = action.sumCredit;
+      state.offChainCheckResult.CPA = action.payload.CPA;
+    },
   },
 });
 
 export default appSlice.reducer;
-export const { setDecodedData, setCertIntegrityCheckResult, setSubjectIntegrityCheckResult } = appSlice.actions;
+export const { setDecodedData, setCertIntegrityCheckResult, setSubjectIntegrityCheckResult, setOffChainCheckResult } = appSlice.actions;
